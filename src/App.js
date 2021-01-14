97114d9a43c89cb768b13fab38c05aac4503f317
Milton Rodrigues <miltonrodrigues713@gmail.com>
2021-01-14 09:12:25 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1> Let's build a Tinder clone</h1>
+    </div>
+  );
+}
+
+export default App;

f4a413094713b41db9c286b1176a7e1c5ef4d244
Milton Rodrigues <miltonrodrigues713@gmail.com>
2021-01-14 12:24:34 -0300

    Implement react-tinder-card

--- src/TinderCard.js
+++ src/TinderCard.js
@@ -1,24 +1,42 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
+import TinderCards from "react-tinder-card";
 
 // Array of objects come from react-tinder-card
 
 function TinderCard() {
-    const [people, setPeople] = useState([
-        {
-            name: 'Holly',
-            url: 'https://cdn11.bigcommerce.com/s-b2hqlmx0cx/images/stencil/1280x1280/products/631/71299/dscf1826_3__81108.1547324237.jpg?c=2'
-        },
-        {
-            name: 'Kendra',
-            url: 'https://i.etsystatic.com/7926516/r/il/caafdf/2304887802/il_794xN.2304887802_a7du.jpg'
-        }
-    ]);    
-    
-    return(
-        <div>
-            <h1>Tinder Card</h1>
-        </div>
-    )
-};
+  const [people, setPeople] = useState([
+    {
+      name: "Holly Madison",
+      url:
+        "https://images-na.ssl-images-amazon.com/images/I/91G0BirVEFL.jpg",
+    },
+    {
+      name: "Kendra Wilkinson",
+      url:
+        "https://i.etsystatic.com/7926516/r/il/caafdf/2304887802/il_794xN.2304887802_a7du.jpg",
+    },
+  ]);
+
+  return (
+    <div>
+      <h1>Tinder Card</h1>
+
+      {people.map((person) => (
+        <TinderCards
+          className="swipe"
+          key={person.name}
+          preventSwipe={['up', 'down']}
+        >
+          <div
+            style={{ backgroundImage: `url(${person.url})` }}
+            className="card"
+          >
+            <h3> {person.name} </h3>
+          </div>
+        </TinderCards>
+      ))}
+    </div>
+  );
+}
 
 export default TinderCard;

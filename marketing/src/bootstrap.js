856f848d22ddcc19a0a42cac2db83f804c0a652c
Miltonr87 <miltonrodrigues713@gmail.com>
2022-12-20 20:55:18 -0300

    Assembling the Marketing App Component

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 // Mount function to start up the app
 const mount = (el) => {
-    ReactDOM.render(
-        <h1>Hi there!</h1>,
-        el
-    )
+    ReactDOM.render(<App />, el)
 }
 
 
